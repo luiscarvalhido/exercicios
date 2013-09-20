aca77838993ad0fe03aa677325341722bdac7381
Nicolas Boarin <nicolas.boarin@gmail.com>
2013-09-20 20:12:50 -0300

    Comentário sobre nome de classes  adicionado

--- contas/Conta.java
+++ contas/Conta.java
@@ -2,6 +2,8 @@ package contas;
 
 public class Conta {
 
+	// De acordo com a Milene é uma má prática de programação colocar o nome da classe no plural.
+
 	public double calculaQuadrado(double x);
 	public double calculaCubo(double x);
 
