3d3e174b085a510812600902fdebaf53bc3fe754
Nicolas Boarin <nicolas.boarin@gmail.com>
2013-09-20 20:11:51 -0300

    Criação do Arquivo

---
+++ contas/ContaNaoConfiavel.java
@@ -0,0 +1,12 @@
+package contas;
+
+public class ContaNaoConfiavel {
+	
+	public double calcularQuadrado( double x ){
+	return x * x;
+	}
+
+	public double calcularCubo( double x ) {
+	return calcularQuadrado(x) * x;
+	}
+}
