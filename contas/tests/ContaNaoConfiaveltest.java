e6313b2e015ad6ca7c6958d4b66d1e08351cc2c4
Nicolas Boarin <nicolas.boarin@gmail.com>
2013-09-20 20:50:27 -0300

    Testes Finais

--- contas/tests/ContaNaoConfiaveltest.java
+++ contas/tests/ContaNaoConfiaveltest.java
@@ -29,6 +29,27 @@ public class ContaNaoConfiavelTest {
                 assertEquals(aConta.calculaQuadrado(0),naoConta.calculaQuadrado(0),0.01);
         }
         
+        @Test
+        public void testQuadradoPositivoPequeno() {
+                assertEquals(aConta.calculaQuadrado(3.1),naoConta.calculaQuadrado(3.1),0.01);
+        }
+        
+        @Test
+        public void testQuadradoNegativoPequeno() {
+                assertEquals(aConta.calculaQuadrado(-5),naoConta.calculaQuadrado(-5),0.01);
+        }
+        
+        @Test
+        public void testQuadradoPositivoGrande() {
+                assertEquals(aConta.calculaQuadrado(1000),naoConta.calculaQuadrado(1000),0.01);
+        }
+        
+        @Test
+        public void testQuadradoNegativoGrande() {
+                assertEquals(aConta.calculaQuadrado(-130),naoConta.calculaQuadrado(-130),0.01);
+        }
+        
+        
         
         
         //Cubo
@@ -36,6 +57,26 @@ public class ContaNaoConfiavelTest {
         @Test
         public void testCuboZero() {
                 assertEquals(aConta.calculaCubo(0),naoConta.calculaCubo(0),0.01);
+        }
+        
+        	@Test
+        public void testCuboPositivoPequeno() {
+                assertEquals(aConta.calculaCubo(5),naoConta.calculaCubo(5),0.01);
+        }
+        
+        @Test
+        public void testCuboNegativoPequeno() {
+                assertEquals(aConta.calculaCubo(-3.1),naoConta.calculaCubo(-3.1),0.01);
+        }
+        
+        @Test
+        public void testCuboPositivoGrande() {
+                assertEquals(aConta.calculaCubo(10000),naoConta.calculaCubo(10000),0.01);
+        }
+        
+        @Test
+        public void testCuboNegativoGrande() {
+                assertEquals(aConta.calculaCubo(-256),naoConta.calculaCubo(-256),0.01);
         }
         
         
