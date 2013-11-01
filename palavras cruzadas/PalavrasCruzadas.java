654941409beb5d084ce133d81c9bf795f2027942
Nicolas Boarin <nicolas.boarin@gmail.com>
2013-11-01 01:25:30 -0200

    Função marcador() criada, classe terminada

--- palavras cruzadas/PalavrasCruzadas.java
+++ palavras cruzadas/PalavrasCruzadas.java
@@ -33,5 +33,64 @@ public class PalavrasCruzadas {
         public void setColuna(int coluna) {
                 this.coluna = coluna;
         }
+        
+        public void marcador(){
+                
+                int i,j;
+                
+                for (i = 0; i < linha; i++)
+                        for (j = 0; j < coluna; j++) {
+                                
+                                if(matriz[i][j]==-1){
+                                        matrizPalavras[i][j]= matriz[i][j];
+                                }
+                                
+                                else if((i==0)){
+                                        if ((matriz[i+1][j]==0)){
+                                                matrizPalavras[i][j] =inicio;
+                                                inicio++;
+                                        }
+                                        else
+                                                matrizPalavras[i][j]=matriz[i][j];
+                                }
+                                
+                                else if((j==0)){
+                                        if( (matriz[i][j+1]==0)){
+                                                matrizPalavras[i][j] =inicio;
+                                                inicio++;
+                                        }
+                                        else
+                                                matrizPalavras[i][j]=matriz[i][j];
+                                }
+                                else if((i==linha-1)){
+                                        if ((matriz[i][j-1]==-1)){
+                                                matrizPalavras[i][j] =inicio;
+                                                inicio++;
+                                        }
+                                        else
+                                                matrizPalavras[i][j]=matriz[i][j];
+                                }
+                                else if((j==(coluna-1))){
+                                        if( (matriz[i-1][j]==-1)){
+                                                matrizPalavras[i][j] =inicio;
+                                                inicio++;
+                                        }
+                                        else
+                                                matrizPalavras[i][j]=matriz[i][j];
+                                }
+                                
+                                else if((matriz[i][j+1]==0) && (matriz[i][j-1]==-1)){
+                                        matrizPalavras[i][j] = inicio;
+                                        inicio++;
+                                }
+                                
+                                else if((matriz[i+1][j]==0) && (matriz[i-1][j]==-1) && i < (linha-1) ){
+                                        matrizPalavras[i][j] = inicio;
+                                        inicioDaPalavra++;
+                                }
+                                else
+                                        matrizPalavras[i][j]= matriz[i][j];
+                        }
+        }
        
 }
