e892dd3ea57a2ea8892eda7e83982a17688c3a2b
Nicolas Boarin <nicolas.boarin@gmail.com>
2013-11-01 01:30:23 -0200

    primeiro commit

---
+++ palavras cruzadas/Test/PalavrasCruzadasTest.java
@@ -0,0 +1,19 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPalavraCruzada {
+        
+        PalavrasCruzadas objetoTeste = new PalavrasCruzadas(5,8);       
+	
+	@Before
+	
+
+        @Test
+        public void test() {
+        	fail("Not yet implemented");
+       	}
+
+}
