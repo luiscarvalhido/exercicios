370d9475df3c18697e339b6834e6dba383b08a2e
Nicolas Boarin <nicolas.boarin@gmail.com>
2013-11-01 01:44:28 -0200

    Criação do método para teste.

--- palavras cruzadas/Tests/PalavrasCruzadasTest.java
+++ palavras cruzadas/Tests/PalavrasCruzadasTest.java
@@ -9,11 +9,20 @@ public class TestPalavraCruzada {
         PalavrasCruzadas main = new PalavrasCruzadas(2,4);       
 	
 	@Before
-	
+	Integer [][] teste = {{0,-1,0,-1},{-1,-1,-1,-1};
+        Integer [][] resposta = {{1,-1,2,-1},{-1,-1,-1,-1}};
 
         @Test
         public void test() {
-        	fail("Not yet implemented");
+        	main.setMatriz(teste);
+                main.marcador();
+                Integer[][] test = main.getMatriz();
+                
+                for (int i = 0; i < main.getLinha(); i++)
+                        for (int j = 0; j < main.getColuna(); j++)
+                                assertEquals(resposta[i][j],test[i][j]);
+
+                }
        	}
 
 }
