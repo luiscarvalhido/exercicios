c53ea645610c289b35cd1dedb3f7355fbe7937c5
Nicolas Boarin <nicolas.boarin@gmail.com>
2013-11-01 01:14:33 -0200

    Criação das matrizes, get/set

--- palavras cruzadas/PalavrasCruzadas.java
+++ palavras cruzadas/PalavrasCruzadas.java
@@ -2,9 +2,21 @@ package palavras;
 
 public class PalavrasCruzadas {
         
+        private Integer[][] matriz, matrizPalavras;
         private int inicio = 1; 
         private int linha,coluna;
         
+        public Integer[][] getMatriz() {
+                return matrizComPalavrasEnumeradas;
+        }
+
+        public void setMatriz(Integer[][] matriz) {
+                this.matriz = matriz;
+        }
+        
+        public int getMatrizPalavras(int i, int j) {
+                return matrizPalavras[i][j];
+        }
 
         public int getLinha() {
                 return linha;
