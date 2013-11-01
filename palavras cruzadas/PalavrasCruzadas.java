fd6473dd77002d443680211a5debae82c088a06e
Nicolas Boarin <nicolas.boarin@gmail.com>
2013-11-01 01:09:59 -0200

    Criação da classe

---
+++ palavras cruzadas/PalavrasCruzadas.java
@@ -0,0 +1,25 @@
+package palavras;
+
+public class PalavrasCruzadas {
+        
+        private int inicio = 1; 
+        private int linha,coluna;
+        
+
+        public int getLinha() {
+                return linha;
+        }
+
+        public void setLinha(int linha) {
+                this.linha = linha;
+        }
+
+        public int getColuna() {
+                return coluna;
+        }
+
+        public void setColuna(int coluna) {
+                this.coluna = coluna;
+        }
+       
+}
